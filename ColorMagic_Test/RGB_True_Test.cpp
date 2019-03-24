ad288b47d8223b491ba676340624910e6d341a06
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-03-24 02:52:29 +0100

    Implement tests for (string) hex code constructor of rgb true color

--- ColorMagic_Test/RGB_True_Test.cpp
+++ ColorMagic_Test/RGB_True_Test.cpp
@@ -45,3 +45,19 @@ TEST_F(RGB_True_Test, From_HexCode_Tests)
 	EXPECT_EQ(*white, *(Color(HexcodeColors::White).get_rgb_true()));
 	EXPECT_EQ(*transparent, *(Color(HexcodeColors::Transparent).get_rgb_true()));
 }
+
+TEST_F(RGB_True_Test, From_HexString_Tests)
+{
+	EXPECT_EQ(*red, *(new rgb_truecolor("#FFFF0000")));
+	EXPECT_EQ(*red, *(new rgb_truecolor("#FF0000")));
+	EXPECT_EQ(*red, *(new rgb_truecolor("FF0000")));
+	EXPECT_THROW(*(new rgb_truecolor("#GGFF0000")), std::invalid_argument);
+	EXPECT_THROW(*(new rgb_truecolor("FF")), std::invalid_argument);
+
+	EXPECT_EQ(*red, *(new rgb_truecolor("FFFF0000")));
+	EXPECT_EQ(*yellow, *(new rgb_truecolor("FFFFFF00")));
+	EXPECT_EQ(*black, *(new rgb_truecolor("FF000000")));
+	EXPECT_EQ(*grey, *(new rgb_truecolor("FF808080")));
+	EXPECT_EQ(*white, *(new rgb_truecolor("FFFFFFFF")));
+	EXPECT_EQ(*transparent, *(new rgb_truecolor("00FFFFFF")));
+}

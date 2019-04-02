5793a6194361fb5c5f75d8f2de966ad529d8b6b7
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-02 23:04:23 +0200

    Implement tests for CIELAB Delta E CIE00 distance calculation

--- ColorMagic_Test/ColorDistance_Test.cpp
+++ ColorMagic_Test/ColorDistance_Test.cpp
@@ -165,3 +165,20 @@ TEST_F(ColorDistance_Test, CIELAB_DeltaE_CIE94)
 
 	EXPECT_NEAR(0.f, color_manipulation::color_distance::cielab_delta_e_cie94(cmyk_yellow, cmyk_yellow), avg_error);
 }
+
+TEST_F(ColorDistance_Test, CIELAB_DeltaE_CIE00)
+{
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(cmyk_yellow, cmyk_red), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(hsv_yellow, hsv_red), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(hsl_yellow, hsl_red), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(xyz_yellow, xyz_red), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(lab_yellow, lab_red), avg_error);
+	EXPECT_NEAR(32.7f, color_manipulation::color_distance::cielab_delta_e_cie00(grey1_d, grey2_d), avg_error);
+	EXPECT_NEAR(32.7f, color_manipulation::color_distance::cielab_delta_e_cie00(grey1_t, grey2_t), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(rgb_d_yellow, rgb_d_red), avg_error);
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(rgb_t_yellow, rgb_t_red), avg_error);
+
+	EXPECT_NEAR(64.3f, color_manipulation::color_distance::cielab_delta_e_cie00(cmyk_yellow, rgb_t_red), avg_error);
+
+	EXPECT_NEAR(0.f, color_manipulation::color_distance::cielab_delta_e_cie00(cmyk_yellow, cmyk_yellow), avg_error);
+}

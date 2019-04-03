e441904b1ab34f470a6ba45e4023ab15dbd2648b
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-04 00:27:00 +0200

    Implement function which increases/decreases saturation in rgb deep space
    Simple calculation that includes perception of the human eye and linear interpolates between colored and grey version of the color.
    Use positive values between 0 and 1 (percentage) to increase saturation
    Use negative values between 0 and -1 (percentage) to decrease saturation

---
+++ ColorMagic/manipulation/color_adjustments.h
@@ -0,0 +1,35 @@
+/// Copyright Martin Ruehlicke, 2019
+/// Use, modification and distribution are subject to the
+/// MIT Software License, Version 1.0.
+/// See accompanying file LICENSE.txt
+
+#pragma once
+
+#include "..\spaces\color_base.h"
+#include "color_converter.h"
+
+//! Static class for color adjustments like desaturation.
+	/*!
+	* This static class implements various functions that modify a given color.
+	*/
+class color_adjustments
+{
+public:
+	//! Static function that increases or decreases the saturation of a given color.
+	/*!
+	* \param color The color to manipulate.
+	* \param percentage The amount by which to adjust the saturation. Values between 0 and 1 
+	* increase the saturation and values between 0 and -1 decrease it.
+	*/
+	static void saturate(color_space::color_base& color, float percentage);
+
+	//! Static function that increases or decreases the saturation of a given color.
+	/*!
+	* \param color The color to manipulate.
+	* \param percentage The amount by which to adjust the saturation. Values between 0 and 1
+	* increase the saturation and values between 0 and -1 decrease it.
+	* \return The modified color in the same color space like the input color.
+	*/
+	static color_space::color_base* saturate(color_space::color_base* color, float percentage);
+};
+

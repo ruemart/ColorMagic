b09811dd272e0791e0d47d8a721de20340c3e58e
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-04 18:39:12 +0200

    Implement tests for rgb luminosity function

--- ColorMagic_Test/ColorAdjustments.cpp
+++ ColorMagic_Test/ColorAdjustments.cpp
@@ -86,3 +86,31 @@ TEST_F(ColorAdjustments_Test, SaturateHSL)
 	EXPECT_NEAR(plus50.green(), plus50_calc.green(), avg_error);
 	EXPECT_NEAR(plus50.blue(), plus50_calc.blue(), avg_error);
 }
+
+TEST_F(ColorAdjustments_Test, LuminateRGB)
+{
+	auto minus100 = rgb_truecolor((unsigned char)0, (unsigned char)0, (unsigned char)0);
+	auto plus50 = rgb_truecolor((unsigned char)192, (unsigned char)0, (unsigned char)0);
+	auto minus50 = rgb_truecolor((unsigned char)128, (unsigned char)0, (unsigned char)0);
+	auto zero = rgb_truecolor((unsigned char)255, (unsigned char)0, (unsigned char)0);
+
+	auto minus100_calc = *(dynamic_cast<rgb_truecolor*>(color_adjustments::luminate_in_rgb_space(rgb_t_red, -1.f)));
+	EXPECT_NEAR(minus100.red(), minus100_calc.red(), avg_error);
+	EXPECT_NEAR(minus100.green(), minus100_calc.green(), avg_error);
+	EXPECT_NEAR(minus100.blue(), minus100_calc.blue(), avg_error);
+
+	auto minus50_calc = *(dynamic_cast<rgb_truecolor*>(color_adjustments::luminate_in_rgb_space(rgb_t_red, -0.5f)));
+	EXPECT_NEAR(minus50.red(), minus50_calc.red(), avg_error);
+	EXPECT_NEAR(minus50.green(), minus50_calc.green(), avg_error);
+	EXPECT_NEAR(minus50.blue(), minus50_calc.blue(), avg_error);
+
+	auto zero_calc = *(dynamic_cast<rgb_truecolor*>(color_adjustments::luminate_in_rgb_space(rgb_t_red, 0.f)));
+	EXPECT_NEAR(zero.red(), zero_calc.red(), avg_error);
+	EXPECT_NEAR(zero.green(), zero_calc.green(), avg_error);
+	EXPECT_NEAR(zero.blue(), zero_calc.blue(), avg_error);
+
+	auto plus50_calc = *(dynamic_cast<rgb_truecolor*>(color_adjustments::luminate_in_rgb_space(&minus50_calc, 0.5f)));
+	EXPECT_NEAR(plus50.red(), plus50_calc.red(), avg_error);
+	EXPECT_NEAR(plus50.green(), plus50_calc.green(), avg_error);
+	EXPECT_NEAR(plus50.blue(), plus50_calc.blue(), avg_error);
+}

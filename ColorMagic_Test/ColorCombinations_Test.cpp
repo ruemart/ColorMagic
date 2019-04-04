2677080c490c7e0b4e8083926ff4281354db0c74
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-04 20:48:34 +0200

    Implement tests for triplet function

--- ColorMagic_Test/ColorCombinations_Test.cpp
+++ ColorMagic_Test/ColorCombinations_Test.cpp
@@ -11,17 +11,24 @@ protected:
 	float avg_error = 0.05f;
 
 	rgb_truecolor *rgb_t_red;
+	rgb_truecolor *rgb_t_green;
+	rgb_truecolor *rgb_t_blue;
 	rgb_truecolor *rgb_t_cyan;
 
 	virtual void SetUp()
 	{
 		rgb_t_red = new rgb_truecolor((unsigned char)255, (unsigned char)0, (unsigned char)0);
+		rgb_t_green = new rgb_truecolor((unsigned char)0, (unsigned char)255, (unsigned char)0);
+		rgb_t_blue = new rgb_truecolor((unsigned char)0, (unsigned char)0, (unsigned char)255);
 		rgb_t_cyan = new rgb_truecolor((unsigned char)0, (unsigned char)255, (unsigned char)255);
 	}
 
 	virtual void TearDown()
 	{
 		delete rgb_t_red;
+		delete rgb_t_green;
+		delete rgb_t_blue;
+		delete rgb_t_cyan;
 	}
 };
 
@@ -31,4 +38,22 @@ TEST_F(ColorCombinations_Test, Complimentary)
 	EXPECT_NEAR(rgb_t_cyan->red(), red_complimentary->red(), avg_error);
 	EXPECT_NEAR(rgb_t_cyan->green(), red_complimentary->green(), avg_error);
 	EXPECT_NEAR(rgb_t_cyan->blue(), red_complimentary->blue(), avg_error);
+}
+
+TEST_F(ColorCombinations_Test, Triplet)
+{
+	auto red_triplet = color_manipulation::color_combinations::create_triplet(rgb_t_red);
+
+	EXPECT_EQ(3, red_triplet.size());
+	EXPECT_NEAR(rgb_t_red->red(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[0])->red(), avg_error);
+	EXPECT_NEAR(rgb_t_red->green(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[0])->green(), avg_error);
+	EXPECT_NEAR(rgb_t_red->blue(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[0])->blue(), avg_error);
+
+	EXPECT_NEAR(rgb_t_green->red(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[1])->red(), avg_error);
+	EXPECT_NEAR(rgb_t_green->green(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[1])->green(), avg_error);
+	EXPECT_NEAR(rgb_t_green->blue(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[1])->blue(), avg_error);
+
+	EXPECT_NEAR(rgb_t_blue->red(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[2])->red(), avg_error);
+	EXPECT_NEAR(rgb_t_blue->green(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[2])->green(), avg_error);
+	EXPECT_NEAR(rgb_t_blue->blue(), dynamic_cast<color_space::rgb_truecolor*>(red_triplet[2])->blue(), avg_error);
 }

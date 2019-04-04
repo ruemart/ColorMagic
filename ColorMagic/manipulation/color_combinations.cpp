9f016e7aefc3fda78efe859e89a1b6c7596c38c3
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-05 01:22:50 +0200

    Throw invalid_argument if number of colors in create_combination method is outside [2,359]
    Maybe go back to clamping. I am not sure what is better.

--- ColorMagic/manipulation/color_combinations.cpp
+++ ColorMagic/manipulation/color_combinations.cpp
@@ -21,16 +21,20 @@ std::vector<color_space::color_base*> color_manipulation::color_combinations::cr
 	return create_combination(color, 5);
 }
 
-std::vector<color_space::color_base*> color_manipulation::color_combinations::create_combination(color_space::color_base * color, int number_of_elements)
+std::vector<color_space::color_base*> color_manipulation::color_combinations::create_combination(color_space::color_base * color, int number_of_colors)
 {
 	std::vector<color_space::color_base*> combination;
 	combination.push_back(color);
 
-	number_of_elements = max(min(360, number_of_elements), 0);
-	auto degrees = 360.f / number_of_elements;
+	if (number_of_colors == 0) throw new std::invalid_argument("A color combination with 0 colors in it is invalid. Chose a color count >= 2");
+	if (number_of_colors == 1) throw new std::invalid_argument("A color combination with 1 colors in it is invalid since it would just return the input color. Chose a color count >= 2");
+	if (number_of_colors > 360) throw new std::invalid_argument("A color combination with more than 360 colors is invalid.");
+
+	//number_of_colors = max(min(360, number_of_colors), 2); // Using exceptions instead of clamp. Maybe change back to clamp? I am unsure
+	auto degrees = 360.f / number_of_colors;
 	auto color_hsl = dynamic_cast<color_space::hsl*>(color_manipulation::color_converter::convertTo(color, color_type::HSL));
 
-	for (auto i = 1; i <= number_of_elements - 1; ++i)
+	for (auto i = 1; i <= number_of_colors - 1; ++i)
 	{
 		auto new_color = new color_space::hsl(color_hsl->hue() + (i * degrees), color_hsl->saturation(), color_hsl->lightness());
 		combination.push_back(color_manipulation::color_converter::convertTo(new_color, color->get_color_type()));

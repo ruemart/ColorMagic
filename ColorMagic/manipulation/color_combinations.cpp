ac7b4c6d6c1db1fb01f0e47a0bf71460ab6d2ed4
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-04 20:48:12 +0200

    Create color triplets based on the input color
    The input color gets converted to hsl and 120 is added to hue two times. The resulting colors are converted back to input color space.

--- ColorMagic/manipulation/color_combinations.cpp
+++ ColorMagic/manipulation/color_combinations.cpp
@@ -9,3 +9,19 @@ color_space::color_base * color_manipulation::color_combinations::get_compliment
 
 	return color_manipulation::color_converter::convertTo(color_hsl, color->get_color_type());
 }
+
+std::vector<color_space::color_base*> color_manipulation::color_combinations::create_triplet(color_space::color_base * color)
+{
+	std::vector<color_space::color_base*> color_triplet;
+	color_triplet.push_back(color);
+
+	auto color_hsl = dynamic_cast<color_space::hsl*>(color_manipulation::color_converter::convertTo(color, color_type::HSL));
+
+	for (auto i = 1; i <= 2; ++i)
+	{
+		auto new_color = new color_space::hsl(color_hsl->hue() + (i * 120.f), color_hsl->saturation(), color_hsl->lightness());
+		color_triplet.push_back(color_manipulation::color_converter::convertTo(new_color, color->get_color_type()));
+	}
+
+	return color_triplet;
+}
--- ColorMagic/manipulation/color_combinations.h
+++ ColorMagic/manipulation/color_combinations.h
@@ -2,6 +2,7 @@
 
 #include "..\spaces\color_base.h"
 #include "color_converter.h"
+#include <vector>
 
 namespace color_manipulation
 {
@@ -20,5 +21,14 @@ namespace color_manipulation
 		* \return The complimentary color.
 		*/
 		static color_space::color_base* get_complimentary_color(color_space::color_base* color);
+
+		//! Static function that returns a color triplet.
+		/*!
+		* This function converts the given color to hsl space and adds two times 120 degrees to its hue.
+		* Afterwards it converts the new colors back to input color space.
+		* \param color The base color for the triplet.
+		* \return A vector containing all three colors. The input color is at index 0.
+		*/
+		static std::vector<color_space::color_base*> create_triplet(color_space::color_base* color);
 	};
 }

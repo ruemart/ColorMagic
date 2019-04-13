af189d14fc3981d7edbfdcd7dc7ea43c707194f3
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-04-13 14:26:04 +0200

    Add documentation to hsl.h

--- ColorMagic/spaces/hsl.cpp
+++ ColorMagic/spaces/hsl.cpp
@@ -40,19 +40,6 @@ color_space::hsl & color_space::hsl::operator=(const color_space::hsl & other)
 	return *this;
 }
 
-color_space::hsl color_space::hsl::operator+(const hsl & rhs)
-{
-	auto this_color = convert_to_vector_format(*this);
-	auto other_color = convert_to_vector_format(rhs);
-
-	std::vector<float> combined_color;
-	combined_color.push_back((this_color[0] + other_color[0]) / 2); // new x component
-	combined_color.push_back((this_color[1] + other_color[1]) / 2); // new y component
-	combined_color.push_back((this_color[2] + other_color[2]) / 2); // new z component
-
-	return convert_from_vector_format(combined_color);
-}
-
 float color_space::hsl::hue() const
 {
 	return m_component_vector[0];
@@ -87,27 +74,4 @@ float color_space::hsl::lightness() const
 void color_space::hsl::lightness(float new_lightness)
 {
 	set_component(new_lightness, 2);
-}
-
-std::vector<float> color_space::hsl::convert_to_vector_format(const hsl & color)
-{
-	std::vector<float> result;
-
-	result.push_back(cosf(color.hue() / 180.f * M_PI) * color.saturation()); // x component of vector
-	result.push_back(sinf(color.hue() / 180.f * M_PI) * color.saturation()); // y component of vector
-	result.push_back(color.lightness()); // y component of vector
-
-	return result;
-}
-
-color_space::hsl color_space::hsl::convert_from_vector_format(const std::vector<float> color)
-{
-	if (color.size() != 3)
-	{
-		throw new std::invalid_argument("HSL: Error while converting from color vector back to hsl. The vector does not have 3 components.");
-	}
-
-	auto h = atan2f(color[1], color[0]) * 180.f / M_PI;
-	auto s = sqrtf(color[0] * color[0] + color[1] * color[1]);
-	return hsl(h, s, color[2]);
 }

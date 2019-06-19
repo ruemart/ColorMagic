140edc1d72d8dbcb37425aa58de9288876b8ee4f
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-06-20 00:53:25 +0200

    Add matrix template class to store transform matices in rgb color space definition

---
+++ ColorMagic/utils/matrix.cpp
@@ -0,0 +1,224 @@
+﻿#include "stdafx.h"
+
+#ifndef __MATRIX_CPP
+#define __MATRIX_CPP
+
+#include "matrix.h"
+
+template<typename T>
+matrix<T>& matrix<T>::operator=(const matrix<T>& other)
+{
+	if (this == &other) return *this;
+
+	size_t new_rows = other.rows();
+	size_t new_cols = other.columns();
+
+	m_values.resize(new_rows);
+	for (size_t i = 0; i < m_values.size(); i++)
+	{
+		m_values[i].resize(new_cols);
+	}
+
+	for (unsigned i = 0; i < new_rows; i++)
+	{
+		for (unsigned j = 0; j < new_cols; j++)
+		{
+			m_values[i][j] = other(i, j);
+		}
+	}
+	m_rows = new_rows;
+	m_columns = new_cols;
+
+	return *this;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator+(const matrix<T>& other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (unsigned i = 0; i < m_rows; i++)
+	{
+		for (unsigned j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] + other(i, j);
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::operator+=(const matrix<T>& other)
+{
+	matrix result = (*this) + other;
+	(*this) = result;
+	return *this;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator-(const matrix<T>& other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (unsigned i = 0; i < m_rows; i++)
+	{
+		for (unsigned j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] - other(i, j);
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::operator-=(const matrix<T>& other)
+{
+	matrix result = (*this) - other;
+	(*this) = result;
+	return *this;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator*(const matrix<T>& other)
+{
+	size_t rows = other.rows();
+	size_t cols = other.columns();
+	matrix result(rows, cols);
+
+	for (size_t i = 0; i < rows; i++)
+	{
+		for (size_t j = 0; j < cols; j++)
+		{
+			for (size_t k = 0; k < rows; k++)
+			{
+				result(i, j) += this->m_values[i][k] * other(k, j);
+			}
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::operator*=(const matrix<T>& other)
+{
+	matrix result = (*this) * other;
+	(*this) = result;
+	return *this;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator+(const T & other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] + other;
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator-(const T & other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] - other;
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator*(const T & other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] * other;
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T> matrix<T>::operator/(const T & other)
+{
+	matrix result(m_rows, m_columns);
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[i][j] / other;
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+std::vector<T> matrix<T>::operator*(const std::vector<T>& other)
+{
+	std::vector<T> result(other.size());
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result[i] = this->m_values[i][j] * other[j];
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T> matrix<T>::transpose()
+{
+	matrix result(m_rows, m_columns);
+
+	for (size_t i = 0; i < m_rows; i++)
+	{
+		for (size_t j = 0; j < m_columns; j++)
+		{
+			result(i, j) = this->m_values[j][i];
+		}
+	}
+
+	return result;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::insert(std::vector<T>& values)
+{
+	if (values.size() == this->size())
+	{
+		for (size_t i = 0; i < m_rows; i++)
+		{
+			for (size_t j = 0; j < m_columns; j++)
+			{
+				this->m_values[i][j] = values[i * j];
+			}
+		}
+	}
+	return *this;
+}
+
+#endif
---
+++ ColorMagic/utils/matrix.h
@@ -0,0 +1,159 @@
+﻿#ifndef __MATRIX_H
+#define __MATRIX_H
+
+#include <vector>
+
+template <typename T> class matrix
+{
+public:
+	//! Default constructor.
+	/*!
+	* Sets the matrix size (row and column count) to 0.
+	*/
+	matrix()
+	{
+		m_values.resize(0);
+	}
+
+	//! Default constructor for rectangular matrices.
+	/*!
+	* \param rows The number of rows.
+	* \param columns The number of columns.
+	*/
+	matrix(size_t rows, size_t columns) : m_rows(rows), m_columns(columns)
+	{
+		m_values.resize(m_rows);
+		for (size_t i = 0; i < m_values.size(); i++)
+		{
+			m_values[i].resize(m_columns);
+		}
+	}
+
+	//! Default constructor for rectangular matrices.
+	/*!
+	* \param rows The number of rows.
+	* \param columns The number of columns.
+	* \param initial_value The default value to fill the matrix with.
+	*/
+	matrix(size_t rows, size_t columns, T initial_value) : m_rows(rows), m_columns(columns)
+	{
+		m_values.resize(m_rows);
+		for (size_t i = 0; i < m_values.size(); i++)
+		{
+			m_values[i].resize(m_columns, initial_value);
+		}
+	}
+
+	//! Default constructor for quadatic matrices.
+	/*!
+	* \param size The number of rows and columns.
+	*/
+	matrix(size_t size) : m_rows(size), m_columns(size)
+	{
+		m_values.resize(size);
+		for (size_t i = 0; i < m_values.size(); i++)
+		{
+			m_values[i].resize(size);
+		}
+	}
+
+	//! Default constructor for quadatic matrices.
+	/*!
+	* \param size The number of rows and columns.
+	* \param initial_value The default value to fill the matrix with.
+	*/
+	matrix(size_t size, T initial_value) : m_rows(size), m_columns(size)
+	{
+		m_values.resize(size);
+		for (size_t i = 0; i < m_values.size(); i++)
+		{
+			m_values[i].resize(size, initial_value);
+		}
+	}
+
+	//! Default copy constructor.
+	/*!
+	* \param other The matrix to copy.
+	*/
+	matrix(const matrix<T>& other)
+	{
+		m_rows = other.rows();
+		m_columns = other.columns();
+		m_values = other.values();
+	}
+
+	//! Default destructor.
+	~matrix() 
+	{
+	}
+
+	//! Accessment operator
+	T& operator() (size_t row, size_t column) { return m_values[row][column]; }
+	//! Accessment operator
+	const T& operator() (size_t row, size_t column) const { return m_values[row][column]; }
+
+	//! Assignment operator
+	matrix<T>& operator= (const matrix<T>& other);
+
+	//! Add two matrices
+	matrix<T> operator+(const matrix<T>& other);
+	//! Add a matrix to this one
+	matrix<T>& operator+=(const matrix<T>& other);
+	//! Subtract two matrices
+	matrix<T> operator-(const matrix<T>& other);
+	//! Subtract a matrix from this one
+	matrix<T>& operator-=(const matrix<T>& other);
+	//! Multiply two matrices
+	matrix<T> operator*(const matrix<T>& other);
+	//! Multiply a matrix with this one
+	matrix<T>& operator*=(const matrix<T>& other);
+
+	//! Add T to each element of this matrix
+	matrix<T> operator+(const T& other);
+	//! Subtract T from each element of this matrix
+	matrix<T> operator-(const T& other);
+	//! Multiply T with each element of this matrix
+	matrix<T> operator*(const T& other);
+	//! Divide each element of this matrix through T
+	matrix<T> operator/(const T& other);
+	
+	//! Multiply this matrix with a vector.
+	/*!
+	* Multiply this matrix with a vector.
+	* /param other The vector to multiply this matrix with.
+	*/
+	std::vector<T> operator*(const std::vector<T>& other);
+
+	//! Transpose this matrix.
+	/*!
+	* Transpose this matrix. Swaps rows and columns.
+	*/
+	matrix<T> transpose();
+
+	//! Insert a vector of values into this matrix.
+	/*!
+	* Insert a vector of values into this matrix. The 
+	* vector needs to have the same size as this matrix.
+	* Otherwise no change to this matrix is done.
+	* /param values A vector containing the values to insert.
+	*/
+	matrix<T>& insert(std::vector<T>& values);
+
+	//! Return the number of rows this matrix has.
+	size_t rows() const { return m_rows; }
+	//! Return the number of columns this matrix has.
+	size_t columns() const { return m_columns; }
+	//! Return the number of elements this matrix has.
+	size_t size() const { return m_rows * m_columns; }
+	//! Return the values of this matrix as a two dimensional vector.
+	std::vector<std::vector<T>> values() const { return m_values; }
+	
+private:
+	size_t m_rows;
+	size_t m_columns;
+	std::vector<std::vector<T>> m_values;
+};
+
+#include "matrix.cpp"
+
+#endif

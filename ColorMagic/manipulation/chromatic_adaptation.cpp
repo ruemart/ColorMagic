07ba554adc07648022932fd49edf05b0770ca91a
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-07-31 22:28:39 +0200

    Implement von Kries adaptation methods

---
+++ ColorMagic/manipulation/chromatic_adaptation.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "chromatic_adaptation.h"
+
+color_space::color_base * color_manipulation::color_adjustments::von_kries_adaptation(color_space::color_base * color, color_space::rgb_color_space_definition * target_color_space)
+{
+	// Convert to XYZ space
+	auto tmp_color = color_manipulation::color_converter::to_xyz(color);
+		
+	// Transform the input color and create a new xyz space object
+	auto transformed_components = (calculate_transform_matrix(tmp_color->get_rgb_color_space()->get_white_point(), target_color_space->get_white_point(), m_von_kries, m_inverted_von_kries)) * tmp_color->get_component_vector();
+	auto tmp_trans_color = new color_space::xyz(transformed_components[0], transformed_components[1], transformed_components[2], tmp_color->alpha(), target_color_space);
+	
+	// Convert transformed color back to input color space
+	return color_manipulation::color_converter::convertTo(tmp_trans_color, color->get_color_type());
+}
+
+color_space::color_base * color_manipulation::color_adjustments::von_kries_adaptation(color_space::color_base * color, color_space::white_point * target_white_point)
+{
+	auto color_space_def = new color_space::rgb_color_space_definition(color->get_rgb_color_space()->get_red_x(), color->get_rgb_color_space()->get_red_y(),
+		color->get_rgb_color_space()->get_green_x(), color->get_rgb_color_space()->get_green_y(),
+		color->get_rgb_color_space()->get_blue_x(), color->get_rgb_color_space()->get_blue_y(),
+		target_white_point, color->get_rgb_color_space()->get_gamma_curve());
+
+	return von_kries_adaptation(color, color_space_def);
+}
+
+matrix<float> color_manipulation::color_adjustments::calculate_transform_matrix(color_space::white_point * source_wp, color_space::white_point * dest_wp, matrix<float> mat, matrix<float> inverted_mat)
+{
+	// Create scaled white point vectors
+	auto scaled_source_wp = m_von_kries * std::vector<float> { source_wp->get_tristimulus().begin(), source_wp->get_tristimulus().end() };
+	auto scaled_dest_wp = m_von_kries * std::vector<float> { dest_wp->get_tristimulus().begin(), dest_wp->get_tristimulus().end() };
+
+	// Create white point matrix from scaled white point vectors
+	auto wp_matrix = matrix<float>(3, 3, std::vector<float>
+	{
+		scaled_dest_wp[0] / scaled_source_wp[0], 0.f, 0.f,
+		0.f, scaled_dest_wp[1] / scaled_source_wp[1], 0.f, 
+		0.f, 0.f, scaled_dest_wp[2] / scaled_source_wp[2]
+	});
+
+	return inverted_mat * wp_matrix * mat;
+}
--- ColorMagic/manipulation/chromatic_adaptation.h
+++ ColorMagic/manipulation/chromatic_adaptation.h
@@ -18,11 +18,59 @@ namespace color_manipulation
 	class color_adjustments
 	{
 	public:
+		//! Transforms a given color to the given target color space definition by using von Kies method.
+		/*!
+		* Transforms a given color to the given target color space definition. The color will first be converted to xyz space.
+		* Afterwards the transformation is done using von Kies matrices and finally the result is converted back to input color space.
+		* \param color The color to convert. If the color is not in xyz space it will be converted first.
+		* \param target_color_space The target color space definition containing the target white point definition.
+		* \return The transformed color in the input color space.
+		*/
 		color_space::color_base* von_kries_adaptation(color_space::color_base* color, color_space::rgb_color_space_definition* target_color_space);
 
+		//! Transforms a given color with the given target white point by using von Kies method.
+		/*!
+		* Transforms a given color with the given target white point. The color will first be converted to xyz space.
+		* Afterwards the transformation is done using von Kies matrices and finally the result is converted back to 
+		* input color space.
+		* \param color The color to convert. If the color is not in xyz space it will be converted first.
+		* \param target_white_point The target white point.
+		* \return The transformed color in the input color space.
+		*/
 		color_space::color_base* von_kries_adaptation(color_space::color_base* color, color_space::white_point* target_white_point);
 	protected:
-		matrix<float>* m_von_kries = new matrix<float>(3, 3, std::vector<float> { 0.40024f, 0.7076f, -0.08081f, -0.2263f, 1.16532f, 0.0457f, 0.f, 0.f, 0.91822f });
-		matrix<float>* m_inverted_von_kries = new matrix<float>(3, 3, std::vector<float> { 1.8599364f, -1.1293816f, 0.2198974f,	0.3611914f, 0.6388125f, -0.0000064f, 0.f, 0.f, 1.0890636f });
+
+		//! Helper method that calculates the transformation matrix.
+		/*!
+		* Helper method that calculates the transformation matrix.
+		* \param source_wp The input colors white point.
+		* \param dest_wp The target white point.
+		* \param mat The adaptation matrix of the chosen method.
+		* \param inverted_mat The inverted adaptation matrix of the chosen method.
+		* \return The calculated transformation matrix for the chosen method.
+		*/
+		matrix<float> calculate_transform_matrix(color_space::white_point* source_wp, color_space::white_point* dest_wp, matrix<float> mat, matrix<float> inverted_mat);
+
+		//! Adaptation matrix of the von Kries method.
+		/*!
+		* Adaptation matrix of the von Kries method.
+		*/
+		matrix<float> m_von_kries = matrix<float>(3, 3, std::vector<float> 
+		{
+			0.40024f, 0.7076f, -0.08081f, 
+			-0.2263f, 1.16532f, 0.0457f, 
+			0.f, 0.f, 0.91822f
+		});
+
+		//! Inverted adaptation matrix of the von Kries method.
+		/*!
+		* Inverted adaptation matrix of the von Kries method.
+		*/
+		matrix<float> m_inverted_von_kries = matrix<float>(3, 3, std::vector<float> 
+		{
+			1.8599364f, -1.1293816f, 0.2198974f,
+			0.3611914f, 0.6388125f, -0.0000064f,
+			0.f, 0.f, 1.0890636f 
+		});
 	};
 }

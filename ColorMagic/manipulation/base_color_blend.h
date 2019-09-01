7d5b736ffaf75efbbe5bad0a27b7576a88cc6d71
Martin Ruehlicke <martin.ruehlicke@gmail.com>
2019-09-02 00:41:12 +0200

    Extract do_porter_duff method to base class base_color_blend
    - Make do_porter_duff virtual instead of static
    - d and s params are now booleans (false = blank, true = source/destination)
    - Extend region enum to also include all blend modes. Region enum will only be used for both region now and defines which blend function or color to use.

---
+++ ColorMagic/manipulation/base_color_blend.h
@@ -0,0 +1,130 @@
+/// Copyright Martin Ruehlicke, 2019
+/// Use, modification and distribution are subject to the
+/// MIT Software License, Version 1.0.
+/// See accompanying file LICENSE.txt
+
+#pragma once
+
+#include "..\spaces\color_base.h"
+#include "color_converter.h"
+
+namespace color_manipulation
+{
+	//! Base class for porter duff and color blend operations.
+	/*
+	* Abstract base class for porter duff and color blend operations. It offers an implementation of the general
+	* porter duff equation as well as a enum specifying the 4 regions of a resultant pixel. Both contents are 
+	* used by porter_duff and color_blend classes.
+	*/
+	class base_color_blend
+	{
+	protected:
+		enum both_region
+		{
+			SOURCE_COLOR = 0, /*!< SOURCE - the source color will be used for the both region */
+			DESTINATION_COLOR, /*!< DESTINATION - the destination color will be used for the both region */
+			NORMAL_BLEND, /*!< NORMAL_BLEND - Normal blending will be used for the both region */
+			MULTIPLY_BLEND, /*!< MULTIPLY_BLEND - Multiply blending will be used for the both region */
+			SCREEN_BLEND, /*!< SCREEN_BLEND - Screen blending will be used for the both region */
+			OVERLAY_BLEND, /*!< OVERLAY_BLEND - Overlay blending will be used for the both region */
+			DARKEN_BLEND, /*!< DARKEN_BLEND - Darken blending will be used for the both region */
+			LIGHTEN_BLEND, /*!< LIGHTEN_BLEND - Lighten blending will be used for the both region */
+			COLOR_DODGE_BLEND, /*!< COLOR_DODGE_BLEND - Color dodge blending will be used for the both region */
+			COLOR_BURN_BLEND, /*!< COLOR_BURN_BLEND - Color burn blending will be used for the both region */
+			HARD_LIGHT_BLEND, /*!< HARD_LIGHT_BLEND - Hard light blending will be used for the both region */
+			SOFT_LIGHT_BLEND, /*!< SOFT_LIGHT_BLEND - Soft light blending will be used for the both region */
+			DIFFERENCE_BLEND, /*!< DIFFERENCE_BLEND - Difference blending will be used for the both region */
+			EXCLUSION_BLEND, /*!< EXCLUSION_BLEND - Exclusion blending will be used for the both region */
+			BLANK /*!< BLANK - Neither source or destination color will be used for the both region */
+		};
+
+		//! Virtual function that actually does the combination of both colors by using a porter duff operator.
+		/*!
+		* Virtual function that actually does the combination of both colors by using a porter duff operator.
+		* The definition of the operator to use is done with the s, d and b parameters. These parameters
+		* define wether the source, destination or both areas of the resulting pixel should be included in
+		* the operation.
+		* \param source The source color of the operation.
+		* \param destination The destination color of the operation.
+		* \param use_s Whether the source area of the resulting pixel is blank or source color.
+		* \param use_d Whether the destination area of the resulting pixel is blank or destination color.
+		* \param b The both area of the resulting pixel. Use SOURCE to include the source color in the
+		* operation, DESTINATION to include the destination color, NEITHER to exclude both colors or a
+		* blend mode to use special functions for the both area.
+		* \return the combination of source and destination calculated based on the given s, d, b parameters.
+		*/
+		virtual color_space::color_base* do_porter_duff(color_space::rgb_deepcolor* source, color_space::rgb_deepcolor* destination, bool use_s, bool use_d, both_region b)
+		{
+			// Check input params
+			if (source == nullptr) throw new std::invalid_argument("source color is null.");
+			if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
+			if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
+
+			// Calculate area factors
+			float src_area = source->alpha() * (1.f - destination->alpha());
+			float dest_area = destination->alpha() * (1.f - source->alpha());
+			float both_area = source->alpha() * destination->alpha();
+
+			// Calculate resulting alpha value
+			float resulting_alpha = src_area * (use_s ? 1.f : 0.f) + dest_area * (use_d ? 1.f : 0.f) + both_area * ((b == both_region::BLANK) ? 0.f : 1.f);
+
+			// Create a new rgb object for the resulting color
+			color_space::rgb_deepcolor* resulting_color = new color_space::rgb_deepcolor(0.f, resulting_alpha, source->get_rgb_color_space());
+
+			// Calculate the resulting color component wise
+			for (size_t i = 0; i < resulting_color->get_component_vector().size(); ++i)
+			{
+				// Source and destination products
+				float s_product = src_area * (use_s ? source->get_component_vector()[i] : 0.f);
+				float d_product = dest_area * (use_d ? destination->get_component_vector()[i] : 0.f);
+
+				// Both product
+				float b_factor;
+				switch (b)
+				{
+				case both_region::SOURCE_COLOR:
+					b_factor = source->get_component_vector()[i];
+					break;
+				case both_region::DESTINATION_COLOR:
+					b_factor = destination->get_component_vector()[i];
+					break;
+				case both_region::NORMAL_BLEND:
+					break;
+				case both_region::MULTIPLY_BLEND:
+					break;
+				case both_region::SCREEN_BLEND:
+					break;
+				case both_region::OVERLAY_BLEND:
+					break;
+				case both_region::DARKEN_BLEND:
+					break;
+				case both_region::LIGHTEN_BLEND:
+					break;
+				case both_region::COLOR_DODGE_BLEND:
+					break;
+				case both_region::COLOR_BURN_BLEND:
+					break;
+				case both_region::HARD_LIGHT_BLEND:
+					break;
+				case both_region::SOFT_LIGHT_BLEND:
+					break;
+				case both_region::DIFFERENCE_BLEND:
+					break;
+				case both_region::EXCLUSION_BLEND:
+					break;
+				case both_region::BLANK:
+				default:
+					b_factor = 0.f;
+					break;
+				}
+				float b_product = both_area * b_factor;
+
+				// Sum up products and assign them to the resulting color object.
+				resulting_color->set_component(s_product + d_product + b_product, i);
+			}
+
+			resulting_color->alpha_divide();
+			return resulting_color;
+		}
+	};
+}
--- ColorMagic/manipulation/porter_duff.cpp
+++ ColorMagic/manipulation/porter_duff.cpp
@@ -8,7 +8,7 @@ color_space::color_base * color_manipulation::porter_duff::src(color_space::colo
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::NEITHER, region::SOURCE), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, false, both_region::SOURCE_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::dest(color_space::color_base * source, color_space::color_base * destination)
@@ -18,7 +18,7 @@ color_space::color_base * color_manipulation::porter_duff::dest(color_space::col
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::DESTINATION, region::DESTINATION), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, true, both_region::DESTINATION_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::atop(color_space::color_base * source, color_space::color_base * destination)
@@ -28,7 +28,7 @@ color_space::color_base * color_manipulation::porter_duff::atop(color_space::col
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::DESTINATION, region::SOURCE), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, true, both_region::SOURCE_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::dest_atop(color_space::color_base * source, color_space::color_base * destination)
@@ -38,7 +38,7 @@ color_space::color_base * color_manipulation::porter_duff::dest_atop(color_space
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::NEITHER, region::DESTINATION), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, false, both_region::DESTINATION_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::over(color_space::color_base * source, color_space::color_base * destination)
@@ -48,7 +48,7 @@ color_space::color_base * color_manipulation::porter_duff::over(color_space::col
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::DESTINATION, region::SOURCE), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, true, both_region::SOURCE_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::dest_over(color_space::color_base * source, color_space::color_base * destination)
@@ -58,7 +58,7 @@ color_space::color_base * color_manipulation::porter_duff::dest_over(color_space
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::DESTINATION, region::DESTINATION), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, true, both_region::DESTINATION_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::in(color_space::color_base * source, color_space::color_base * destination)
@@ -68,7 +68,7 @@ color_space::color_base * color_manipulation::porter_duff::in(color_space::color
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::NEITHER, region::SOURCE), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, false, both_region::SOURCE_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::dest_in(color_space::color_base * source, color_space::color_base * destination)
@@ -78,7 +78,7 @@ color_space::color_base * color_manipulation::porter_duff::dest_in(color_space::
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::NEITHER, region::DESTINATION), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, false, both_region::DESTINATION_COLOR), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::out(color_space::color_base * source, color_space::color_base * destination)
@@ -88,7 +88,7 @@ color_space::color_base * color_manipulation::porter_duff::out(color_space::colo
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::NEITHER, region::NEITHER), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, false, both_region::BLANK), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::dest_out(color_space::color_base * source, color_space::color_base * destination)
@@ -98,7 +98,7 @@ color_space::color_base * color_manipulation::porter_duff::dest_out(color_space:
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::DESTINATION, region::NEITHER), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, true, both_region::BLANK), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::x_or(color_space::color_base * source, color_space::color_base * destination)
@@ -108,7 +108,7 @@ color_space::color_base * color_manipulation::porter_duff::x_or(color_space::col
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::SOURCE, region::DESTINATION, region::NEITHER), source->get_color_type());
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), true, true, both_region::BLANK), source->get_color_type());
 }
 
 color_space::color_base * color_manipulation::porter_duff::clear(color_space::color_base * source, color_space::color_base * destination)
@@ -118,57 +118,5 @@ color_space::color_base * color_manipulation::porter_duff::clear(color_space::co
 	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
 	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
 
-	return color_converter::convertTo(do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), region::NEITHER, region::NEITHER, region::NEITHER), source->get_color_type());
-}
-
-color_space::color_base * color_manipulation::porter_duff::do_porter_duff(color_space::rgb_deepcolor * source, color_space::rgb_deepcolor * destination, region s, region d, region b)
-{
-	// Check input params
-	if (source == nullptr) throw new std::invalid_argument("source color is null.");
-	if (destination == nullptr) throw new std::invalid_argument("destination color is null.");
-	if (source->get_rgb_color_space() != destination->get_rgb_color_space()) throw new std::invalid_argument("The rgb color space definitions of both colors do not match.");
-	if (s == region::DESTINATION) throw new std::invalid_argument("s param must not be region::DESTINATION.");
-	if (d == region::SOURCE) throw new std::invalid_argument("s param must not be region::SOURCE.");
-
-	// Calculate area factors
-	float src_area = source->alpha() * (1.f - destination->alpha());
-	float dest_area = destination->alpha() * (1.f - source->alpha());
-	float both_area = source->alpha() * destination->alpha();
-
-	// Calculate resulting alpha value
-	float resulting_alpha =	src_area * ((s == region::NEITHER) ? 0.f : 1.f) + dest_area * ((d == region::NEITHER) ? 0.f : 1.f) + both_area * ((b == region::NEITHER) ? 0.f : 1.f);
-
-	// Create a new rgb object for the resulting color
-	color_space::rgb_deepcolor* resulting_color = new color_space::rgb_deepcolor(0.f, resulting_alpha, source->get_rgb_color_space());
-
-	// Calculate the resulting color component wise
-	for (size_t i = 0; i < resulting_color->get_component_vector().size(); ++i)
-	{
-		// Source and destination products
-		float s_product = src_area * ((s == region::SOURCE) ? source->get_component_vector()[i] : 0.f);
-		float d_product = dest_area * ((d == region::DESTINATION) ? destination->get_component_vector()[i] : 0.f);
-
-		// Both product
-		float b_factor;
-		switch (b)
-		{
-		case color_manipulation::porter_duff::SOURCE:
-			b_factor = source->get_component_vector()[i];
-			break;
-		case color_manipulation::porter_duff::DESTINATION:
-			b_factor = destination->get_component_vector()[i];
-			break;
-		case color_manipulation::porter_duff::NEITHER:
-		default:
-			b_factor = 0.f;
-			break;
-		}
-		float b_product = both_area * b_factor;
-
-		// Sum up products and assign them to the resulting color object.
-		resulting_color->set_component(s_product + d_product + b_product, i);
-	}
-
-	resulting_color->alpha_divide();
-	return resulting_color;
+	return color_converter::convertTo((new porter_duff())->do_porter_duff(color_converter::to_rgb_deep(source), color_converter::to_rgb_deep(destination), false, false, both_region::BLANK), source->get_color_type());
 }
